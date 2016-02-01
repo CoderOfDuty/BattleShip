60c3f54504bf3cc8f3c5d8a8ffd997080aa7d254
Xavi P <xpi@edu.tecnocampus.cat>
2016-02-01 13:03:27 +0100

    v1

---
+++ src/battleship/ShipBoard.java
@@ -0,0 +1,50 @@
+package battleship;
+
+public class ShipBoard {
+
+	public final static int BOARDWIDTH = 10;
+
+	private int[][] board;
+	private int firedShips;
+	
+	public int getFiredShips(){
+		return firedShips;
+	}
+	
+	public void moreFiredShips(){
+		firedShips++;
+		System.out.println("suma "+firedShips);
+	}
+
+	public ShipBoard() {
+
+		board = new int[BOARDWIDTH][BOARDWIDTH];
+
+	}
+
+	public int[][] getBoard() {
+		return board;
+	}
+
+	public void setBoardCoords(int x, int y, int value) {
+		board[x][y] = value;
+	}
+
+	public void emptyBoard() {
+
+		for (int x = 0; x < BOARDWIDTH; x++) {
+			for (int y = 0; y < BOARDWIDTH; y++) {
+				board[x][y] = Ship.EMPTY;
+			}
+		}
+	}
+	
+	public boolean stillShips(int shipsCounter) {
+		
+		System.out.println("getFired " + firedShips);
+		System.out.println("count"+ shipsCounter);
+		if(firedShips==shipsCounter)return false;
+		return true;
+	}
+
+}
